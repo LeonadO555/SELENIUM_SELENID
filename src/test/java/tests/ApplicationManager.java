190f36013754d9df250df7fb24f44951b97df928
ViktoriiaPan <you@example.com>
2023-06-26 12:04:58 +0200

    Added new Gradle dependencies, created java class Wait.java, PageBase.java, ApplicationManager.java, TestBase.java, configuration for logs, added and describes LoginPage.java.

---
+++ src/test/java/tests/ApplicationManager.java
@@ -0,0 +1,38 @@
+package tests;
+
+import com.google.common.io.Files;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    public WebDriver driver;
+
+    protected void init() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://jere237.softr.app/sign-in");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    protected void stop() {
+        driver.quit();
+    }
+
+    public String takeScreenshot() throws IOException {
+        File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File screenshot = new File("reference/screen" + System.currentTimeMillis() + ".png");
+
+        Files.copy(tmp, screenshot);
+        return screenshot.getAbsolutePath();
+    }
+
+}
